10d0818c613b84a693467351bb91a0b37532e019
형원 손 <sonwon39@naver.com>
2023-01-14 15:54:35 +0900

    dll test

--- MyProject/source.cpp
+++ MyProject/source.cpp
@@ -1,20 +1,25 @@
 
 #include "Test.h"
 #include <iostream>
+#include "MathLibrary.h"
 using namespace std;
 
 
 using Son::TestClass;
 int main()
 {
-	while (true)
-	{
-		int a;
-		cin >> a;
-		TestClass TC(3);
-		cout << TC.ReturnVal(a) << std::endl;
-		cout << Son::TestAddFunction(3, 4) << std::endl;
-	}
+	int a;
+	cin >> a;
+	TestClass TC(3);
+	cout << TC.ReturnVal(a) << std::endl;
+	cout << Son::TestAddFunction(3, 4) << std::endl;
 	
+	fibonacci_init(1, 1);
+	std::cout << fibonacci_index() << ": "
+		<< fibonacci_current() << std::endl;
+
+	int b;
+	cin >> b;
+
 	return 0;
 }
